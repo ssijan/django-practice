5d57479a7defe8e0fd0987d71a6708d44c83fc15
ssijan <rahman15-5460@diu.edu.bd>
2026-02-22 04:35:51 +0600

    ORM gilli

--- orm/opt/views.py
+++ orm/opt/views.py
@@ -4,12 +4,13 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from weasyprint import HTML
 from django.template.loader import render_to_string
+from django.db.models import Sum
 
 # Create your views here.
 def index(request):
     obj = Student.objects.all().order_by('student_id__student_id')
 
-    paginator = Paginator(obj,25)
+    paginator = Paginator(obj,15)
     page_number = request.GET.get('page',1)
     page_obj = paginator.get_page(page_number)
 
@@ -31,13 +32,24 @@ def cal_GPA(x):
         return 'D'
     else:
         return 'F'
+    
 
+def get_rankTotal(st_id):
+
+    total = Student.objects.filter(student_id__student_id = st_id)\
+    .aggregate(total_mark = Sum('studentmarks__mark'))['total_mark']
+
+    rank = Student.objects.annotate(total_mark = Sum('studentmarks__mark'))\
+        .filter(total_mark__gt = total).count() + 1
+    
+    return total, rank
 
 def result(request):
     student = None
     mark = None
     error = None
-
+    total = None
+    rank = None
     if request.method == 'POST':
         student_id = request.POST.get('student_id')
 
@@ -48,17 +60,20 @@ def result(request):
             for m in mark:
                 m.grade = cal_GPA(m.mark)
 
+            total , rank = get_rankTotal(student_id)
         except Student.DoesNotExist:
             error = 'Student Not Found'
 
     return render(request, 'result.html', {
         'student': student,
         'marks': mark,
-        'error': error
+        'error': error,
+        'total':total,
+        'rank': rank,
     })
 
 
-
+# Download Result in PDF
 def result_pdf(request, student_id):
     # Fetch the student and marks
     student = get_object_or_404(Student, student_id__student_id=student_id)
@@ -67,10 +82,14 @@ def result_pdf(request, student_id):
     for m in marks:
         m.grade = cal_GPA(m.mark)
 
+    total , rank = get_rankTotal(student_id)
+
     # Render HTML template to string
     html_string = render_to_string('result_pdf.html', {
         'student': student,
-        'marks': marks
+        'marks': marks,
+        'total':total,
+        'rank': rank,
     })
 
     # Generate PDF
